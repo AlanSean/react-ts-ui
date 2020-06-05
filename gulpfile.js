f082015be834e4e1fd69d32ce81a63d0feae0702
Mr.huang <2316694914@qq.com>
2020-06-05 19:17:15 +0800

    添加 es模块

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,54 @@
 const gulp = require("gulp"),
 	ts = require("gulp-typescript"),
-	tsConfig = ts.createProject("tsconfig.json"),
 	less = require("gulp-less"),
 	autoprefixer = require("gulp-autoprefixer"),
-	babelConfig = require('./babelConfig.js')(),
-	libDir="./lib",
+	babelConfig = require('./babelConfig.js')(false),
+	libDir = "./lib",
+	esDir="./es",
+	,
 	babel = require('gulp-babel');
-
-
+	
+	lib_tsConfig =getTsConfig(true);
+	es_tsConfig =getTsConfig();
 
 const tsResult = gulp.src([
 	'src/**/*.tsx',
 	'src/**/*.ts',
-	'./typings.d.ts'
-]).pipe(tsConfig());
-
-gulp.task('ts', done => {
+	'typings.d.ts'
+]);
+function getTsConfig(module){
 	delete babelConfig.cacheDirectory;
-	tsResult.js.pipe(babel(babelConfig)).pipe(gulp.dest(libDir));
+	tsConfig = require('./tsConfig');
+	if(module){
+		tsConfig.module= "commonjs";
+	} else {
+		tsConfig.moduleResolution= "node";
+	}
+	return tsResult.pipe(ts.createProject(tsConfig)());
+}
+gulp.task('lib-ts', done => {
+	lib_tsConfig.js.pipe(babel(babelConfig)).pipe(gulp.dest(libDir));
 	done()
 });
-gulp.task('dts', done => {
-	tsResult.dts.pipe(gulp.dest(libDir));
+gulp.task('lib-tsd', done => {
+	lib_tsConfig.dts.pipe(gulp.dest(libDir));
+	done()
+});
+gulp.task('es-ts', done => {
+	es_tsConfig.js.pipe(babel(babelConfig)).pipe(gulp.dest(esDir));
+	done()
+});
+gulp.task('es-tsd', done => {
+	es_tsConfig.dts.pipe(gulp.dest(esDir));
 	done()
 });
 gulp.task('less', done => {
 	gulp.src('src/**/*.less')
 		.pipe(autoprefixer())
 		.pipe(less())
-        .pipe(gulp.dest(libDir));
+		.pipe(gulp.dest(libDir));
 	done()
 });
-gulp.task('default', gulp.series('ts','dts','less',done => {
+gulp.task('default', gulp.series('lib-ts', 'lib-tsd','es-ts', 'es-tsd', 'less', done => {
 	done();
 }));
---
+++ tsConfig.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "noUnusedParameters": true,
+  "target": "es6",
+  "sourceMap": true,
+  "jsx": "preserve",
+  "allowSyntheticDefaultImports": true,
+  "forceConsistentCasingInFileNames": true,
+  "noImplicitReturns": false,
+  "suppressImplicitAnyIndexErrors": true,
+  "noUnusedLocals": true,
+  "allowJs": true,
+  "skipLibCheck": true,
+  "experimentalDecorators": true,
+  "strict": true,
+  "allowUnreachableCode": false,
+  "strictNullChecks": true,
+  "declaration": true,
+  "noImplicitAny": true,
+  "baseUrl": ".",
+  "paths": {
+      "@/*": ["./src/*"]
+  }
+}
